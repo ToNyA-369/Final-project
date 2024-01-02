a4e284375a103f64fee031b9e0221e799eaab687
cug43 <yoyojan0403@gmail.com>
2024-01-02 16:20:54 +0800

    cursor

--- Final project/Final project.cpp
+++ Final project/Final project.cpp
@@ -1,6 +1,27 @@
 ﻿#include <stdio.h>
+#include <Windows.h>
+
+void HideCursor(void);
+void CursorJump(int x, int y);
 
 int main()
 {
-    
+	HideCursor();
 }
+
+void HideCursor(void)
+{
+	CONSOLE_CURSOR_INFO curInfo;
+	curInfo.dwSize = 1;
+	curInfo.bVisible = FALSE;
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleCursorInfo(handle, &curInfo);
+}
+
+void CursorJump(int x, int y)
+{
+	COORD pos; pos.X = x; pos.Y = y;
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleCursorPosition(handle, pos);
+}
+
